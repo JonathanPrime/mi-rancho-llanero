38b48f6e15e8de0dfbd620606852bec2ca9df131
= <=>
2022-06-16 00:17:14 -0500

    update celular

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ var owl = $('.owl-carousel').owlCarousel({
    let msg = document.getElementById('whats-in').value;
    let relmsg = msg.replace(/ /g,"%20");
      //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
-    window.open('https://wa.me/573228929177?text='+relmsg, '_blank'); 
+    window.open('https://wa.me/573132286949?text='+relmsg, '_blank'); 
    
    });
  
